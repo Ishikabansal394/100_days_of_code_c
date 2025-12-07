38df672434a95a8264c872db3004c0f953dcaba0
Ishika Bansal <ishikabansal0224@gmail.com>
2025-12-07 12:17:11 +0530

    Implement repeated element finder in Q119_.c

--- Q119_.c
+++ Q119_.c
@@ -19,3 +19,33 @@ Output 3:
 1
 
 */
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int visited[n];
+    for(int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+    int repeat = -1;
+    for(int i = 0; i < n; i++) {
+        if(visited[arr[i]] == 1) {
+            repeat = arr[i];
+            break;
+        }
+        visited[arr[i]] = 1;
+    }
+
+    printf("Repeated element = %d", repeat);
+    return 0;
+}
