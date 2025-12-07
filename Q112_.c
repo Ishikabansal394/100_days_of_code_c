5d06269d98c55fe3ecebfce28ee7f4e806305b2d
Ishika Bansal <ishikabansal0224@gmail.com>
2025-12-07 12:16:34 +0530

    Correct maximum sum calculation in Q112_.c
    Fix logic for calculating maximum sum in array.

--- Q112_.c
+++ Q112_.c
@@ -20,3 +20,25 @@ Output 3:
 25
 
 */
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = arr[0], max = arr[0];
+    for(int i = 1; i < n; i++) {
+        sum = sum + arr[i] > arr[i] ? sum + arr[i] : arr[i];
+        max = max > sum ? max : sum;
+    }
+
+    printf("Maximum sum = %d", max);
+    return 0;
+}
