629280642abdf6711022f725b5527e576a6fe363
ishikabansal394 <ishikabansal0224@gmail.com>
2026-01-05 20:10:56 +0530

    introduction to repo

---
+++ intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    printf("Welcome to 100 Days of Code in C!\n");
+    printf("Let's code together and improve our skills!\n");
+    printf("Icreated this repo as a part of my 100 Days of Code challenge given by my college.\n");
+    return 0;
+}
